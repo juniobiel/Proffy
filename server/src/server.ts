b139cd612ce9e7e0f1b18e8169eb469ab10517e3
Gabriel Junio <gabriel.junio@rocketmail.com>
2020-08-07 04:02:26 -0300

    aula 2 concluida

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,13 @@
-console.log('Hello world 2');
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+
+
+app.listen(3333);
